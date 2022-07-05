a1c9852b59f68f2f13babcb32df449d457ca71ca
victortan <vtan8090@gmail.com>
2022-07-05 16:33:59 -0400

    Added css for login page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,12 +2,17 @@ import SignupForm from "../../components/SignupForm/SignupForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import './AuthPage.css'
 
-export default function AuthPage({setUser}){
-    return(
-        <main>
-            <h1>AuthPage</h1>
-            <SignupForm setUser={setUser}/>
-            <LoginForm setUser={setUser}/>
-        </main>
+export default function AuthPage({ setUser }) {
+    return (
+        <>
+            <div className="top-header"></div>
+            <header>
+                <span>Dogvana</span>
+            </header>
+            <main className="auth-page">
+                <SignupForm setUser={setUser} />
+                <LoginForm setUser={setUser} />
+            </main>
+        </>
     )
 }
