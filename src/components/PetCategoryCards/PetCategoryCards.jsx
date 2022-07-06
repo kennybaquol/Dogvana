648a3bf2ad446b2e7adb3a9a725e17af1afe74b4
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-06 11:59:52 -0400

    Links are added for remaining 3 PetCategoryCards

--- src/components/PetCategoryCards/PetCategoryCards.jsx
+++ src/components/PetCategoryCards/PetCategoryCards.jsx
@@ -4,18 +4,24 @@ import { Link } from 'react-router-dom'
 export default function PetCategoryCards() {
   return (
     <>
-        <header>
-            <h1>Find your new best friend</h1>
-            <span>Browse pets from over 11,500 shelters and rescues.</span>
-        </header>
-        <div className="pet-category-cards">
-            <span>
-            <Link to="/animalCards/dog">Dog</Link>
-              </span>
-            <span>Cat</span>
-            <span>Other Animals</span>
-            <span>Shelters and Rescues</span>
-        </div>
+      <header>
+        <h1>Find your new best friend</h1>
+        <span>Browse pets from over 11,500 shelters and rescues.</span>
+      </header>
+      <div className="pet-category-cards">
+        <span>
+          <Link to="/animalCards/dog">Dog</Link>
+        </span>
+        <span>
+          <Link to="/animalCards/cat">Cat</Link>
+        </span>
+        <span>
+          <Link to="/animalCards/other">Other Animals</Link>
+        </span>
+        <span>
+          <Link to="/animalCards/shelters">Shelters and Rescues</Link>
+        </span>
+      </div>
     </>
   )
 }
