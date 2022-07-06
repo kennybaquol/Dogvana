75b61503ff476d5bbbfd552f16cb3b7daad87885
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-06 14:51:33 -0400

    Got search bar to re-route to search page while passing the search terms in the URL

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,50 +5,21 @@ import './SearchBar.css'
 export default function SearchBar() {
 
     const [newSearch, setNewSearch] = useState('')
-    // State for animals to be listed on the home page -KB
-    const [currentAnimals, setCurrentAnimals] = useState([])
-
-    // useEffect for fetching -KB
-    useEffect(function () {
-        async function getAnimals() {
-            // *REPLACE THE BELOW LINE WITH API FETCH* -KB
-            const animals = ['cat', 'dawg', 'butta dog']
-
-            // fetch(url, {
-            //     method: "GET",
-            //     mode: "no-cors",
-            //     headers: {
-            //         Accept: "application/json",
-            //         Authorization: `Bearer ${token}`
-            //     }
-            // })
-            //     .then(res => res.json())
-            //     .then((data) => {
-            //         console.log(data)
-            //     })
-            //     .catch((error) => {
-            //         console.log(error)
-            //     })
-
-            setCurrentAnimals(animals)
-        }
-        getAnimals();
-    }, []);
-
 
     function handleChange(event) {
         setNewSearch({ ...newSearch, [event.target.name]: event.target.value })
+        // console.log({newSearch})
     }
 
     return (
         <div className="primary-search-bar">
             <form>
-                <input type="text" placeholder="Search Terrier, Kitten, etc." onChange={handleChange} />
-                <input type="text" placeholder="Enter your zip/address" onChange={handleChange} />
-                <Link className="magnifying-glass" to="/search">🔍</Link>
+                <input type="text" name="animal" placeholder="Search Terrier, Kitten, etc." onChange={handleChange} />
+                <input type="text" name="zip code" placeholder="Enter your zip/address" onChange={handleChange} />
+                <Link className="magnifying-glass" to={"search/" + newSearch.animal}>
+                    <button type="submit">🔍</button>
+                </Link>
                 <br />
-                {/* BELOW IS TEMPORARY, REPLACE WITH ANIMALCARDCOMPONENT -KB */}
-                {currentAnimals.join(',')}
             </form>
         </div>
     )
