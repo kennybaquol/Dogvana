1c545e2182dd0a4942b58d21bcf0755603c14166
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-06 15:46:35 -0400

    Added zip to search page url separated by +

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -8,15 +8,14 @@ export default function SearchBar() {
 
     function handleChange(event) {
         setNewSearch({ ...newSearch, [event.target.name]: event.target.value })
-        // console.log({newSearch})
     }
 
     return (
         <div className="primary-search-bar">
             <form>
                 <input type="text" name="animal" placeholder="Search Terrier, Kitten, etc." onChange={handleChange} />
-                <input type="text" name="zip code" placeholder="Enter your zip/address" onChange={handleChange} />
-                <Link className="magnifying-glass" to={"search/" + newSearch.animal}>
+                <input type="text" name="zip" placeholder="Enter your zip/address" onChange={handleChange} />
+                <Link className="magnifying-glass" to={"search/" + newSearch.animal + "+" + newSearch.zip}>
                     <button type="submit">🔍</button>
                 </Link>
                 <br />
