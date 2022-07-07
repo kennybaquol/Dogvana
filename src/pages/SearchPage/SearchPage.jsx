34f3145b3adbf16d398e33f3af5b6e1d5e618202
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-07 11:33:24 -0400

    SearchPage grabs url and stores it to a string/state

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,26 +1,58 @@
-import {useState} from 'react'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import Filter from '../../components/Filter/Filter'
 import PetCard from '../../components/PetCard/PetCard'
 
-export default function SearchPage({ showAnimals }){
+export default function SearchPage({ showAnimals }) {
     const [filter, setFilter] = useState({
         breed: '',
         age: '',
         gender: '',
         sortBy: ''
     })
+    const location = useLocation()
+    const [type, setType] = useState('')
+    // Left temporarily empty for testing -KB
     const animal = []
 
     function handleChange(event) {
-        
+
     }
 
-    // Grab params from url to change the state of filter when the page renders
+    // When page first renders, set the state of type using url params
+    useEffect(() => {
+        (async () => {
+            let searchTerm = location.pathname
+            searchTerm = searchTerm.slice(8)
+            let spaceCounter = 0
+            for (let i = 0; i < searchTerm.length; i++) {
+                if (searchTerm.includes('%20')) {
+                    searchTerm = searchTerm.replace('%20', ' ')
+                }
+            }
+            setType(searchTerm)
+        })()
+    }, [])
 
-    return(
+    useEffect(() => {
+        (async () => {
+            console.log('Type is now: ' + type)
+        })()
+    }, [type])
+
+
+    // Then pass the info down through map to the PetCard component
+
+    return (
         <>
-            <Filter filter={filter}/>
-            <PetCard animal={animal}/>
+            {/* {animalType.length > 0 ?
+                {() => {console.log(animalType)}}
+            :
+            <h3></h3>
+            } */}
+
+            <Filter filter={filter} />
+            <PetCard animal={animal} />
         </>
     )
 }
