f3867aec799d3aa28ba949a64a66b19dfb1de45e
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-11 00:19:38 -0400

    Fixed merge issue

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -1,40 +1,42 @@
 import * as usersAPI from './users-api'
 import jwt_decode from 'jwt-decode'
 
-export async function signUp(userData){
+export async function signUp(userData) {
     const token = await usersAPI.signUp(userData)
     localStorage.setItem('token', token)
 
     return getUser()
 }
 
-export async function login(credentials){
+export async function login(credentials) {
     const token = await usersAPI.login(credentials)
     localStorage.setItem('token', token)
     return getUser()
 }
 
-export function getToken(){
+export function getToken() {
     const token = localStorage.getItem('token')
     if (!token) return null
-    const payload = JSON.parse(atob(token.split('.')[1]))
-    if (payload.exp < Date.now() / 1000){
+    const payload = jwt_decode(token)
+
+    if (payload.exp < Date.now() / 1000) {
         localStorage.removeItem('token')
         return null
     }
     return token
 }
 
-export function getUser(){
+export function getUser() {
     const token = getToken()
-    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+    return token ? jwt_decode(token)
+        : null;
 }
 
-export function logOut(){
+export function logOut() {
     localStorage.removeItem('token')
 }
 
-export async function checkToken(){
+export async function checkToken() {
     return usersAPI.checkToken()
-    .then(dateStr => new Date(dateStr))
+        .then(dateStr => new Date(dateStr))
 }
