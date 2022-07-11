019387886822e66e3cbe874fa290f7d4a3dfaa99
victortan <vtan8090@gmail.com>
2022-07-10 23:36:25 -0400

    merge

--- models/user.js
+++ models/user.js
@@ -33,17 +33,8 @@ const userSchema = new Schema({
         trim: true,
         minLength: 3,
         required: true
-<<<<<<< HEAD
-    }, 
-        favorites: [{
-            ObjectID: String
-        }],
-        
-    
-=======
     },
     favorites: [{ type: favoritesSchema }]
->>>>>>> main
 }, {
     timestamps: true,
     toJSON: {
